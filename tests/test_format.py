e6e9121ead7de733505cb2db28caf52dea03e01f
zhouyijiaren <956952610@qq.com>
2023-02-02 15:01:49 +0800

    add implement

--- tests/test_format.py
+++ tests/test_format.py
@@ -6,9 +6,7 @@ import unittest
 import uuid
 
 from format import encode_header, decode_header, encode_kv, decode_kv
-
-# TODO: use correct value
-HEADER_SIZE: typing.Final[int] = 0
+from format import HEADER_SIZE
 
 
 def get_random_header() -> tuple[int, int, int]:
